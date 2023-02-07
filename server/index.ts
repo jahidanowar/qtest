778921475db41467d4a09e92aca051e883c022ff
Nick <nick@ondrako.com>
2023-02-07 14:13:06 -0500

    db update

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,14 @@ import mongoose from 'mongoose';
 import { Nitro } from 'nitropack';
 
 export default async (_nitroApp: Nitro) => {
-	const config = useRuntimeConfig();
+	// const config = useRuntimeConfig();
 
 	try {
 		mongoose.set('strictQuery', false);
 		await mongoose
-			.connect(config.mongoDB)
+			.connect(
+				'mongodb+srv://Nick:lE9dpJSMpxan5HLO@rsaquote.jhogufy.mongodb.net/quote?retryWrites=true&w=majority'
+			)
 			.then(() => console.log('MongoDB is connected'));
 	} catch (error) {
 		console.log(error);
