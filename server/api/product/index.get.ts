b9afbeb6aa60b90cc1f12ba8c732a8498ec8ead4
Nick <nick@ondrako.com>
2023-02-08 05:57:15 -0500

    product populate

--- server/api/product/index.get.ts
+++ server/api/product/index.get.ts
@@ -2,7 +2,16 @@ import Product from '~/server/models/Product.model';
 
 export default defineEventHandler(async (event) => {
 	try {
-		const products = await Product.find();
+		const products = await Product.find().populate([
+			{
+				path: 'models',
+				model: 'Toughbook',
+			},
+			{
+				path: 'options',
+				model: 'Option',
+			},
+		]);
 
 		return products;
 	} catch (error) {
