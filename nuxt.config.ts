063d609111e4c211f4fa276b387d3a2615789dbe
jahidanowar <milon5143@gmail.com>
2023-02-09 18:29:37 +0530

    Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		head: {
-			title: 'RSA Quote Builder',
-			meta: [
-				{
-					name: 'viewport',
-					content: 'width=device-width, initial-scale=1',
-				},
-				{
-					charset: 'utf-8',
-				},
-			],
-			link: [
-				{
-					rel: 'icon',
-					type: 'image/png',
-					href: 'https://res.cloudinary.com/dywpw9ram/image/upload/v1675299037/Assets/favicon_eyhhfk.png',
-				},
-			],
-		},
-	},
+  app: {
+    head: {
+      title: "RSA Quote Builder",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "https://res.cloudinary.com/dywpw9ram/image/upload/v1675299037/Assets/favicon_eyhhfk.png",
+        },
+      ],
+    },
+  },
 
-	nitro: {
-		plugins: ['~/server/index.ts'],
-	},
+  nitro: {
+    plugins: ["~/server/index.ts"],
+  },
 
-	runtimeConfig: {
-		mongoDB: process.env.MONGO_URI,
-	},
+  runtimeConfig: {
+    mongoDB: process.env.MONGO_URI,
+  },
 
-	modules: [
-		[
-			'@pinia/nuxt',
-			{
-				autoImports: [
-					// automatically imports `defineStore`
-					'defineStore', // import { defineStore } from 'pinia'
-					// automatically imports `defineStore` as `definePiniaStore`
-					['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-				],
-			},
-		],
-	],
-	css: [
-		'vuetify/lib/styles/main.sass',
-		'@mdi/font/css/materialdesignicons.min.css',
-	],
-	build: {
-		transpile: ['vuetify'],
-	},
+  modules: [
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          "defineStore", // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+      },
+    ],
+  ],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
 });
--- server/api/product/[id]/index.get.ts
+++ server/api/product/[id]/index.get.ts
@@ -1,37 +1,37 @@
-import Product from '~/server/models/Product.model';
+import Product from "~/server/models/Product.model";
 
 export default defineEventHandler(async (event) => {
-	try {
-		const { id } = event.context.params;
+  try {
+    const { id } = event.context.params;
 
-		const product = await Product.findOne({ _id: id }).populate(
-			'toughbooks options'
-		);
+    const product = await Product.findOne({ _id: id }).populate(
+      "toughbooks options"
+    );
 
-		// const product = await Product.aggregate([
-		// 	{ $match: { name: 'Toughbook 55' } },
-		// 	{
-		// 		$group: {
-		// 			_id: null,
-		// 			data: { $first: '$$ROOT' },
-		// 		},
-		// 	},
-		// 	{
-		// 		$lookup: {
-		// 			from: 'data.toughbooks',
-		// 			localField: 'toughbooks',
-		// 			foreignField: '_id',
-		// 			as: 'models',
-		// 		},
-		// 	},
-		// ]);
+    // const product = await Product.aggregate([
+    // 	{ $match: { name: 'Toughbook 55' } },
+    // 	{
+    // 		$group: {
+    // 			_id: null,
+    // 			data: { $first: '$$ROOT' },
+    // 		},
+    // 	},
+    // 	{
+    // 		$lookup: {
+    // 			from: 'data.toughbooks',
+    // 			localField: 'toughbooks',
+    // 			foreignField: '_id',
+    // 			as: 'models',
+    // 		},
+    // 	},
+    // ]);
 
-		// console.log(product);
+    console.log({ product });
 
-		return product;
+    return product;
 
-		// const product = await Product.findOne({ _id: id });
-	} catch (error) {
-		console.log(error);
-	}
+    // const product = await Product.findOne({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
 });
